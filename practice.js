e27f0df9e0dfd64d197d8e950ae3764bc8461f9e
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-02-26 15:18:39 +0000

    readme.md added

---
+++ practice.js
@@ -0,0 +1,32 @@
+// Declaring a Variable
+var myName;
+
+/*When JavaScript variables are declared, they have an initial value of undefined. If you do a mathematical operation on an undefined variable your result will be NaN which means "Not a Number". If you concatenate a string with an undefined variable, you will get a literal string of "undefined".*/
+
+//  Assigning value to a variable after you initially declared above
+myName = "Abdalla";
+
+// or declare and assign all at the same line
+
+var lastName = "H Jibril";
+
+/*in Javascript all variable and function names are case sensative. So CAPITALAZATION AMTTER. Write variable names in JavaScript in camelCase.*/
+
+// Add two numbers with JavaScript
+var addTwoNumbers = 5 +3;  // assigns 8 to myVar
+
+//Subtract numbers in Javascript
+
+var newCost =30 -12; 
+
+//Multiply two Numbers
+
+var multiply = 5 * 5; // assigns 25 to multiply
+
+//  Divide inJavaScript
+
+var divideTwoNumbers = 25 / 5;
+
+
+
+
