bdc579a229e8f22bd6b113b9ad45de7681f334a3
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-03-04 18:30:52 +0000

    JS object tutorial ends.

--- build-js-object.js
+++ build-js-object.js
@@ -345,7 +345,44 @@ var myPlant = [
 var secondTree = myPlant[1].list[1];
 console.log(secondTree);
 
+// Record Collection
+var collection = {
+    2548: {
+        albumTitle: "slippery when wet",
+        artist: "Bon Jovi",
+        tracks: ["Let It Rock", "You Give Love a Bad Name"]
+    },
+    2468:{
+        albumTitle: "1999",
+        artist: "Prince",
+        tracks: ["1999", "Little Red Corvett"]
+    },
+    1245: {
+        artist: "Robert Palmer",
+        tracks: []
+    },
+    5439: {
+        albumTitle: "ABBA GOLD"
+    }
+};
 
+function updateRecords(object, id, prop, value) {
+    if (prop !== "tracks" && value !=="") {
+        object[id][prop]= value;
+    }
+    else if (prop == "tracks" && object[id].hasOwnProperty("tracks") === false) {
+        object[id][prop] = value;
+    }
+    else if (prop ===  "tracks" && value !== "") {
+        object[id][prop].push(value);
+    }
+    else if (value === "") {
+        delete boject[id][prop];
+    }
+    return object;
+
+}
+console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
 
 
 
