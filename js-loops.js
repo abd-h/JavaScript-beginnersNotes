744265cde424fe9c3edbb86fbec42e16a127ff78
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-03-12 00:34:41 +0000

    Added more js-loopsn  practice

--- js-loops.js
+++ js-loops.js
@@ -176,14 +176,40 @@ When we execute the while loop, the condition evaluates to false because i is no
  * Replace Loops using Recursion
 Recursion is the concept that a function can be expressed in terms of itself. To help understand this, start by thinking about the following task: multiply the first n elements of an array to create the product of those elements. Using a for loop, you could do this:
  */
+function multiply(arr, n) {
+    product = 1;
+    for (var i = 0; i < n; i++){
+        product *= arr[i];
+    }
+    return product;
+}
+console.log(multiply(2));
 
 
 
 /**
  * However, notice that ... multiply(arr, n) == multiply(arr, n - 1) * arr[n - 1]. That means you can rewrite multiply in terms of itself and never need to use a loop.
- */
+ */ function multiplier(arr, n) {
+     if (n <= 0) {
+         return 1;
+     }
+     else {
+         return multiplier(arr, n-1) * arr[n-1];
+     }
+ }
+console.log(multiplier([2, 3, 4, 5], 3));
 
-
+/*Write a recursive function, sum(arr, n), 
+that returns the sum of the first n elements of an array arr.*/
+function sum(arr, n) {
+    if (n === 0){
+        return 0;
+    }
+    else {
+        return sum(arr, n-1) + arr[n -1];
+    }
+}
+console.log(sum([2, 3, 4, 5], 3));
 /**
  * Recursion practice
  */
