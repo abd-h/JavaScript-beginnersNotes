708aba28152bd87fbf70da3a62340fcafef5daec
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-03-20 23:23:49 +0000

    added more regular expression

--- regular-expressions/regexp2.js
+++ regular-expressions/regexp2.js
@@ -24,3 +24,38 @@ let quoteSample4 ="The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /\w/g;
 let result3 = quoteSample4.match(alphabetRegexV2).length;
 console.log(result3);
+
+/**         Match Everything But Letters and Numbers
+You've learned that you can use a shortcut to match alphanumerics [A-Za-z0-9_] using \w. A natural pattern you might want to search for is the opposite of alphanumerics.
+
+You can search for the opposite of the \w with \W. Note, the opposite pattern uses a capital letter. This shortcut is the same as [^A-Za-z0-9_].
+
+let shortHand = /\W/;
+let numbers = "42%";
+let sentence = "Coding!";
+numbers.match(shortHand);
+sentence.match(shortHand);
+The first match call would return the value ["%"] and the second would return ["!"].
+
+Use the shorthand character class \W to count the number of non-alphanumeric characters in
+various quotes and strings.
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;*/
+let quoteSample5 = "The five boxing wizards jump quickly.";
+let nonAlphabetregex = /\W/g;
+let resultNoNum = quoteSample5.match(nonAlphabetregex).length;
+console.log(resultNoNum);
+
+/*          Match All Numbers
+You've learned shortcuts for common string patterns like alphanumerics. Another common pattern is looking for just digits or numbers.
+
+The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0-9], which looks for a single character of any number between zero and nine.
+
+Use the shorthand character class \d to count how many digits are in movie titles. Written out numbers ("six" instead of 6) do not count.
+
+*/
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/; 
+let movieResult = movieName.match(numRegex).length;
