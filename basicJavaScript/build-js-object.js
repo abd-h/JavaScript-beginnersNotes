e4de3b6324895055a94880feb2826fe17a3cbb6e
abd-h <78439349+abd-h@users.noreply.github.com>
2021-05-11 08:25:54 +0100

    Update build-js-object.js

--- basicJavaScript/build-js-object.js
+++ basicJavaScript/build-js-object.js
@@ -100,14 +100,14 @@ var entreeValue = testObject1["an entree"];
 var drinkValue = testObject1["the drink"];
 console.log(entreeValue, drinkValue);
 
-            /* Accessing Objects with Properties with Veriables
+            /* Accessing Objects Properties with Veriables
  
 Another  use of bracket notation on objects is access a property 
 which is stored as value of veriable. This can be very useful 
 for iterating through an object's properties or when accessing 
 look up table.
 
-     Eample 1 */
+     Example 1 */
 
 var dogs = {
     Fido: "Mutt",
@@ -346,7 +346,10 @@ var gloveBoxContent = myStorage.car.inside["glove box"];
 console.log(gloveBoxContent);
 
 /*          Accessing Nested Arrays
-As we have seen in earlier examples, objects can contain both nested objects and nested arrays. Similar to accessing nested objects, array bracket notation can be chained to access nested arrays.*/ 
+As we have seen in earlier examples, objects can contain both 
+nested objects and nested arrays. Similar to accessing nested 
+objects, array bracket notation can be chained to access 
+nested arrays.*/ 
 
 // Example
 
