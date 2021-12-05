d707454f7374534081a081ff6ed1e7aa8d001404
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-12-05 01:09:57 +0000

    added functional programming

--- functional-programing/index.js
+++ functional-programing/index.js
@@ -132,7 +132,9 @@ const tea4GreenTeamFCC = dynamicChoiceOfTea(prepareTeaType(`Black Tea`), 27);
 const tea4BlackTeamFCC = dynamicChoiceOfTea(prepareTeaType("Green Tea"), 13);
 
 console.log(tea4GreenTeamFCC);
-console.log(tea4BlackTeamFCC);/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+console.log(tea4BlackTeamFCC);
+/*************************************************
+ *************************************************
 
         3.          Understanding The Hazard of Using Imperative Code
         
@@ -186,7 +188,9 @@ Window.prototype.tabOpen = function(tab){
     this.tabs.push("new tab") // lets you open a new tab for now
     return this;
 }
-
+     /********* ********* ********* ********
+     *                  CHALLENGE THREE                *
+    * ********* ********* ********* ******/
 // When you close a tab
 Window.prototype.tabClose = function(index) {
     const tabsBeforeIndex = this.tabs.slice(0, index);// Get the tabs before the tab
@@ -213,7 +217,7 @@ const finalTabs = socialWindow
 console.log(finalTabs.tabs);                
 
 /*************************************************
- *************************************************
+ ************************************************* 
  
 
  Avoid Mutations and Side Effects Using Functional Programming
@@ -228,5 +232,57 @@ console.log(finalTabs.tabs);
 
  The previous example didn't have any complicated operations, but the splice method change the original array, and resulted in a bug.
 
- 
+Recall that in funtional programming, change or altering things is called mutation, and the outcome is called a side effect. A function, ideally, should be a pure function, meaning that it will not cause side effects.
+
+Let's try to master this discipline and not alter any variable or object in our code.
+
+    ********* ********* ********* ********
+    *                  CHALLENGE FOUR                 *
+    * ********* ********* ********* ******
+
+Fill in the code for the function incremener so it returs a the value of the global variable fixedValue incremented by one.
  */
+
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+    let count = fixedValue;
+   
+}
+console.log(incrementer());
+console.log(incrementer());
+/*************************************************
+ ************************************************
+ 
+Pass Arguments to Avoid Extenal Dependance in a Function
+
+The last challenge was a step closer to functional programming principles, but there's still something missing.
+
+We didn't alter the global variable value, but the function incrementer would not work without the global variable fixedValue being there.
+
+Anothe principle of functional programming is to always declare your dependencies explicitly. This means if a function depends on a variable or object being present, then pass that variable or object directly into the function as an argument.
+
+There are several good consequences from this principle.
+The function is easier to test, you know exactly what input it takes, and it won,t on anything else in your program.
+
+This can give more confindence when you alter, remove, or add new code. You would know what you can or cannot change and you can see where the potential traps are.
+
+Finally, the function wouldl always produce the same output for the same set of outputs, no matter what part of the code executes it.
+
+    ********* ********* ********* *********
+    *                  CHALLENGE FIVE                 *
+    * ********* ********* ********* *******
+Let's update the incrementer function to clearly declare its dependencies.
+
+Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
+
+*/
+
+let fixedValue2 = 4;
+function incrementer2(number) {
+    number++
+    return number;
+}
+console.log(incrementer2(fixedValue2));
+console.log(fixedValue2);
